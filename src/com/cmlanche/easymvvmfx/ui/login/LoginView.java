c66035803f156dffd7a7208ea8b39b9b2a5433b2
chengming <chengming@testin.cn>
2018-07-18 14:03:51 +0800

    修改一下测试界面

--- src/com/cmlanche/easymvvmfx/ui/login/LoginView.java
+++ src/com/cmlanche/easymvvmfx/ui/login/LoginView.java
@@ -1,20 +1,74 @@
 package com.cmlanche.easymvvmfx.ui.login;
 
 import com.fx.base.mvvm.BaseView;
+import com.sun.javafx.scene.traversal.Algorithm;
+import com.sun.javafx.scene.traversal.Direction;
+import com.sun.javafx.scene.traversal.ParentTraversalEngine;
+import com.sun.javafx.scene.traversal.TraversalContext;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 import tray.notification.NotificationType;
 import tray.notification.TrayNotification;
+
+import java.math.BigDecimal;
 
 /**
  * Created by cmlanche on 2016/12/9.
  */
 public class LoginView extends BaseView<LoginViewModel> {
 
+    @FXML
+    Button btn;
+    @FXML
+    TextField t1;
+    @FXML
+    TextField t2;
+    @FXML
+    TextField t3;
+    @FXML
+    VBox testbox;
+
 
     @Override
     protected void onViewCreated() {
 
+        BigDecimal b = new BigDecimal(204632000);
+        System.out.println(b.doubleValue());
+
+        BigDecimal a = new BigDecimal(b.doubleValue() - 100);
+        System.out.println(a.toPlainString());
+
+        t2.setFocusTraversable(true);
+        t2.requestFocus();
+
+        testbox.setImpl_traversalEngine(new ParentTraversalEngine(testbox, new Algorithm() {
+            @Override
+            public Node select(Node owner, Direction dir, TraversalContext context) {
+                if ("t2".equals(owner.getId())) {
+                    return t3;
+                } else if ("t3".equals(owner.getId())) {
+                    return t1;
+                } else if ("t1".equals(owner.getId())) {
+                    return btn;
+                } else {
+                    return t2;
+                }
+            }
+
+            @Override
+            public Node selectFirst(TraversalContext context) {
+                return t2;
+            }
+
+            @Override
+            public Node selectLast(TraversalContext context) {
+                return t1;
+            }
+        }));
     }
 
     @FXML
@@ -28,5 +82,6 @@ public class LoginView extends BaseView<LoginViewModel> {
         tray.setMessage(message);
         tray.setNotificationType(notification);
         tray.showAndWait();
+
     }
 }
